595158380e67ce6448e457626b5771085d2d0daf
guldlossen <mads_so@hotmail.com>
2015-04-23 10:52:35 +0200

    FTW

---
+++ MiniTestProject/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTestProject
+{
+    class Customer
+    {
+        string name;
+        DateTime dayOfBirth;
+        string street;
+        int zipcode;
+        string city;
+        string email;
+        int phoneNr;
+    }
+}
